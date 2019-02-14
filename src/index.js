a11ef7bbb9bab2f779fee2e005f6e044a3665de7
rykyou <rkim1214@gmail.com>
2019-02-14 10:46:39 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -5,28 +5,6 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Header />
-        <Counter />
-      </div>
-    );
-  }
-}
-
-class Header extends Component {
-  render() {
-    return (
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome to React</h1>
-      </header>
-    );
-  }
-}
-
-class Counter extends Component {
   state = { count: 0 };
 
   increment = () => {
@@ -37,19 +15,42 @@ class Counter extends Component {
     this.setState(prevState => ({ count: prevState.count - 1 }));
   };
 
+  render() {
+    return (
+      <div className="App">
+        <Header count={this.state.count}/>
+        <Counter count={this.state.count} increment={this.increment} decrement={this.decrement}/>
+      </div>
+    );
+  }
+}
+
+class Header extends Component {
   renderDescription = () => {
-    const remainder = this.state.count % 5;
+    const remainder = this.props.count % 5;
     const upToNext = 5 - remainder;
-    return `The current count is less than ${this.state.count + upToNext}`;
+    return `The current count is less than ${this.props.count + upToNext}`;
   };
 
   render() {
     return (
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">{this.renderDescription()}</h1>
+      </header>
+    );
+  }
+}
+
+class Counter extends Component {
+
+  render() {
+    return (
       <div className="Counter">
-        <h1>{this.state.count}</h1>
-        <button onClick={this.decrement}> - </button>
-        <button onClick={this.increment}> + </button>
-        <h3>{this.renderDescription()}</h3>
+        <h1>{this.props.count}</h1>
+        <button onClick={this.props.decrement}> - </button>
+        <button onClick={this.props.increment}> + </button>
+
       </div>
     );
   }
